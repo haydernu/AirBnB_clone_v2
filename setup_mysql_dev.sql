d205d7f7462ed646def7d136e6b3607877bce1d9
BENOP 7 <benjaminpetegnong@gmail.com>
2023-01-05 14:01:00 +0000

    task 3 AirBnb clone mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- setup database by creating a new database and user
 -- granting privileges to my newly created user
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
