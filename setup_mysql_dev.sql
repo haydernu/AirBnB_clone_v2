b749ef94ee8266ea1955d37b6de66b2fbee772a8
BENOP 7 <benjaminpetegnong@gmail.com>
2023-01-05 13:47:45 +0000

    task 3 AirBnb clone mysql and add gitignore

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- setup database by creating a new database and user
+-- granting privileges to my newly created user
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
