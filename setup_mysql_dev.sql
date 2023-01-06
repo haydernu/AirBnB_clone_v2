26cdb68ca78c928a5d7194b7cd55d3e0c0827ff7
BENOP 7 <benjaminpetegnong@gmail.com>
2023-01-06 20:40:38 +0000

    correct task 3 and push task 4'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,5 @@
 -- granting privileges to my newly created user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
