5bb4777f1e3dcec742e2bfb963e84c1d3f574086
BENOP 7 <benjaminpetegnong@gmail.com>
2023-01-06 22:01:52 +0000

    attempt task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        obj_dict = {}
+        if cls is None:
+            return FileStorage.__objects
+        for k, v in enumerate(FileStorage.__objects):
+            if v.to_dict()[__class__] == cls:
+                obj_dict.update({v.to_dict()['__class__'] + '.' + v.id: v})
+        return obj_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
