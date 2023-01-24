0656ad8e162ef99816110675f2bfd8f66eb321a4
haydernu <haydernuredin2212@gmail.com>
2023-01-24 04:27:19 -0800

    correction

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import *
 from datetime import datetime
 import os
 
-env.hosts = ["54.82.172.96", "100.25.141.81"]
+env.hosts = ["54.83.172.96", "100.25.141.81"]
 env.user = "ubuntu"
 
 def do_pack():
